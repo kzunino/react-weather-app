7da25bb8f2588f1e02427d181b78c7805520dc16
Kyle <kzunino@gmail.com>
2020-02-23 22:25:17 -0800

    removed dependencies

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/global.css";
 
-//import apiKey from './config'
-
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Switch,
-// } from 'react-router-dom';
 
 import Home from './components/Home'
-//import Forecast from "./components/Forecast";
 
 
 export default class App extends Component {
